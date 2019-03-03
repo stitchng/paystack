a706e95389027af1ad8d5fecb424360b58cb7a07
StitchNG <stitchnig@gmail.com>
2019-03-03 11:40:19 +0100

    Update PayStack.js

--- src/PayStack.js
+++ src/PayStack.js
@@ -31,14 +31,15 @@ const apiEndpoints = {
 
 	/*
 	List customers
-	@params: 
+	@params: perPage, page
 	*/
 	listCustomers: {
 		method: 'GET',
 		path: '/customer',
 		send_json: true,
-		params: {},
-		route_params: {}
+		params: { perPage: Number, page: Number },
+		param_defaults: { perPage: 50, page: 1 },
+		route_params: null
 	},
 
 	/*
@@ -53,11 +54,15 @@ const apiEndpoints = {
 		route_params: { customer_id: String }
 	},
 	
+	/*
+	 Initialize Transaction
+	 @params: reference, callback_url, amount, email, plan, invoice_limit
+	*/
 	initializeTransaction:{
 		method:'POST',
 		path:'/transaction/initialize',
 		send_json: true,
-		params: { reference: String, callback_url: String, amount$: Number, email: String, plan: String, invoice_limit: Number, metadata: String },
+		params: { reference: String, callback_url: String, amount$: Number, email$: String, plan: String, invoice_limit: Number, metadata: String },
 		param_defaults: { invoice_limit: 0 },
 		route_params: null
 	},
