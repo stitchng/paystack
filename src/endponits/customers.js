79defe256c33d722cf2437fd8f6db56d7605ae0c
StitchNG <stitchnig@gmail.com>
2019-03-05 12:23:10 +0100

    Create customers.js

---
+++ src/endponits/customers.js
@@ -0,0 +1,53 @@
+
+module.exports = {
+      /*
+   	 Create customer
+   	 @params: first_name, last_name, email, phone
+   	 */
+	createCustomer: {
+		method: 'POST',
+		path: '/customer',
+		send_json: true,
+		params: { first_name: String, last_name: String, email$: String, phone: String },
+		route_params: null
+	},
+
+	/*
+	Get customer
+ 	 @params: customer_id
+	*/
+	getCustomer: {
+		method: 'GET',
+		path: '/customer/{:customer_id}',
+		send_json: true,
+		params: null,
+		route_params: { customer_id: String }
+	},
+
+	/*
+	List customers
+	@params: perPage, page
+	*/
+	listCustomers: {
+		method: 'GET',
+		path: '/customer',
+		send_json: true,
+		params: { perPage: Number, page: Number },
+		param_defaults: { perPage: 50, page: 1 },
+		route_params: null
+	},
+
+	/*
+	Update customer
+	@params: first_name, last_name, email (required), phone
+	*/
+	updateCustomer: {
+		method: 'PUT',
+		path: '/customer/{:customer_id}',
+		send_json: true,
+		params: { first_name: String, last_name: String, email$: String, phone: String },
+		route_params: { customer_id: String }
+	},
+	
+	
+}
