67df43113b75f128be702f25426fa48bcaf51e33
StitchNG <stitchnig@gmail.com>
2019-03-05 12:53:43 +0100

    Update settlements.js

--- src/endpoints/settlements.js
+++ src/endpoints/settlements.js
@@ -1,5 +1,16 @@
 'use strict'
 
 module.exports = {
-
+  /*
+	Get Settlements
+	@params: from, to, subaccount
+	*/
+	getSettlements:{
+		method:'GET',
+		path:'/settlement',
+		send_json: true,
+		params:{from: Date, to: Date, subaccount: String },
+		param_defaults: { subaccount:'none' },
+		route_params: null
+	},
 }
