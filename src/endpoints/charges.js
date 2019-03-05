4da3489866b76eb7fee66a005932ef3dbeb1d78a
StitchNG <stitchnig@gmail.com>
2019-03-05 13:29:31 +0100

    Update charges.js

--- src/endpoints/charges.js
+++ src/endpoints/charges.js
@@ -1,5 +1,27 @@
 'use strict'
 
 module.exports = {
-
+    /*
+   Charge Card
+   @param: card, metadata, reference, amount, email
+  */
+	chargeCard: {
+		method: 'POST',
+		path: '/charge',
+		send_json: true,
+		params: { card$: Object, metadata: Object, reference: String, amount: Number, email: String },
+		route_params: null
+	},
+  
+  /*
+   Charge Bank
+   @param: bank, metadata, reference, amount, email
+  */
+	chargeBank: {
+		method: 'POST',
+		path: '/charge',
+		send_json: true,
+		params: { bank$: Object, metadata: Object, reference: String, amount: Number, email: String },
+		route_params: null
+	}
 }
