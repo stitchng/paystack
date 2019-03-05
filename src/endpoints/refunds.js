99b15a27928ecc36ddc0b646721676e0c1f6da18
StitchNG <stitchnig@gmail.com>
2019-03-05 13:21:20 +0100

    Update refunds.js

--- src/endpoints/refunds.js
+++ src/endpoints/refunds.js
@@ -1,5 +1,41 @@
 'use strict'
 
 module.exports = {
+    /*
+   Create Refund
+   @param: transaction<reference>, amount, currency, customer_note, merchant_note
+  */
+	createRefund: {
+		method: 'POST',
+		path: '/refund',
+		send_json: true,
+		params: { transaction$: String, amount: Number, currency: String, customer_note: String, merchant_note: String },
+    param_defaults:{ currency: 'NGN' },
+		route_params: null
+	},
 
+  /*
+   List Refund
+   @param: reference, currency
+  */
+	listRefund: {
+		method: 'GET',
+		path: '/refund',
+		send_json: false,
+		params: { reference$: String, currency: String },
+    param_defaults:{ currency: 'NGN' },
+		route_params: null
+	},
+  
+  /*
+   Fetch Refund
+   @param: reference, currency
+  */
+	fetchRefund: {
+		method: 'GET',
+		path: '/refund/{:reference}',
+		send_json: false,
+		params: null,
+		route_params: { reference: String }
+	}
 }
