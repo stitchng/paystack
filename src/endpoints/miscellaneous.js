d0f86ace461aed40f80332b71f9960868242734b
StitchNG <stitchnig@gmail.com>
2019-03-05 13:30:22 +0100

    Create miscellaneous.js

---
+++ src/endpoints/miscellaneous.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  /*
+   List Card
+   @param: perPage, page
+  */
+	listBanks:{
+		method:'GET',
+		path:'/bank',
+		send_json: true,
+		params: { perPage: Number, page: Number },
+		param_defaults: { perPage: 50, page: 1 },
+		route_params: null
+	}
+}
