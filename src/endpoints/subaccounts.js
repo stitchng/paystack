52ea087c190bc89e13eb197174b1d10191f2a67b
StitchNG <stitchnig@gmail.com>
2019-03-06 12:08:31 +0100

    Update subaccounts.js

--- src/endpoints/subaccounts.js
+++ src/endpoints/subaccounts.js
@@ -1,5 +1,15 @@
 'use strict'
 
 module.exports = {
-
+    /*
+    
+    */
+    createSubaccount:{
+       method:'POST',
+       path:'/subaccount',
+       send_json: true,
+       params:{ business_name$: String, settlement_bank: String, account_number$: String, percentage_charge$: Number, primary_contact_email: String, primary_contact_name: String, primary_contact_phone: String, metadata: String, settlement_schedule: String },
+       param_defaults:{ settlement_schedule: 'auto', percentage_charge: 0 },
+       route_params: null
+    }
 }
