674de78389a052718eb8cb68e1022d9871c4732b
StitchNG <stitchnig@gmail.com>
2019-03-06 11:59:15 +0100

    Update subscriptions.js

--- src/endpoints/subscriptions.js
+++ src/endpoints/subscriptions.js
@@ -1,5 +1,15 @@
 'use strict'
 
 module.exports = {
-
+    /*
+      Create Subscription
+      @param: customer, plan, authorization, start_date
+    */
+    createSubscription:{
+      method:'POST',
+      plan:'/subscription',
+      send_json: true,
+      params:{ customer$: String, plan$: String, authorization$: String, start_date: Date },
+      route_params: null
+    }
 }
