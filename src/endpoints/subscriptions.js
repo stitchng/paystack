08b913bccbb78fa262ce18677d5e4c4fd83ff29a
devamaz <ahmad.abdulaziz37@gmail.com>
2019-03-09 18:27:26 +0100

    add listsubscription to subscription

--- src/endpoints/subscriptions.js
+++ src/endpoints/subscriptions.js
@@ -11,5 +11,19 @@ module.exports = {
       send_json: true,
       params:{ customer$: String, plan$: String, authorization$: String, start_date: Date },
       route_params: null
+    },
+
+     /*
+      List Subscription
+      @param: perPage, page, customer, plan
+    */
+    listSubscription:{
+      method:'GET',
+      path:'/subscription',
+      send_json: false,
+      params:{ perPage : Number, page: Number, customer: Number, plan: Number},
+      param_defaults:{perPage : 50, page: 1, customer: 0, plan: 0},
+      route_params: null
     }
+
 }
