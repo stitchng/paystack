6cb2f1dced938f6ee788743f203814184e27c094
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2021-02-24 21:03:03 +0100

    Update Mockable.js

--- src/PayStack/extension/Mockable.js
+++ src/PayStack/extension/Mockable.js
@@ -1,8 +1,29 @@
 'use strict'
 
+const payStackMockFactory = require('./facade/MockFactory.js')
+
 class Mockable {
-    static engageMock(){ this.prototype._mock = {} }
-    static disengageMock(){ this.prototype._mock = null }
+  static engageMock () {
+    this.prototype._mock = payStackMockFactory.make(
+      this.prototype.httpClientBaseOptions.hooks,
+      Object.keys(this.prototype)
+    )
+  }
+
+  static disengageMock () {
+    this.prototype._mock = null
+  }
+
+  static mockMacro (methodName, methodRoutine) {
+    if (typeof this.prototype[methodName] !== 'function') {
+      throw new Error('Cannot monkey-patch non-existing methods');
+    }
+
+    if (typeof methodRoutine !== 'function') {
+      throw new Error('Second argument MUST be a function');
+    }
+    return (this.prototype._mock[methodName] = methodRoutine)
+  }
 }
 
 Mockable.prototype._mock = null;
